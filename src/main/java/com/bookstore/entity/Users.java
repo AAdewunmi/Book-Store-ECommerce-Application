00f17f21732e7c74be92ad06dd7326e252176af3
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-05-11 07:46:40 +0100

    Eighth Commit: Implement Users class including Hibernate mappings

--- src/main/java/com/bookstore/entity/Users.java
+++ src/main/java/com/bookstore/entity/Users.java
@@ -1,5 +1,58 @@
 package com.bookstore.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Users {
+	
+	private Integer userId;
+	private String email;
+	private String fullName;
+	private String password;
+	
+	public Users() {
+		super();
+	}
+	
+	@Column(name = "user_id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Column(name = "full_name")
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 
 }
