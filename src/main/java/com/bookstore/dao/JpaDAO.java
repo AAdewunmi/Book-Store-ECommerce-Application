b325e8d9f8999e22261325fcbf521932551fd6e2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-05-17 16:17:41 +0100

    Twenty-Seven Commit: Implement JpaDAO abstract class

--- src/main/java/com/bookstore/dao/JpaDAO.java
+++ src/main/java/com/bookstore/dao/JpaDAO.java
@@ -1,5 +1,22 @@
 package com.bookstore.dao;
 
-public interface JpaDAO {
+import jakarta.persistence.EntityManager;
 
+public class JpaDAO<T>{
+	
+	protected EntityManager entityManager;
+
+	public JpaDAO(EntityManager entityManager) {
+		super();
+		this.entityManager = entityManager;
+	}
+	
+	public T create(T t) {
+		entityManager.getTransaction().begin();
+		entityManager.persist(t);
+		entityManager.flush();
+		entityManager.refresh(t);
+		entityManager.getTransaction().commit();
+		return t;
+	}
 }
