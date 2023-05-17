fc6719caf9e6b9f97dd939ab65248a5215b4f7ef
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-05-17 15:47:54 +0100

    Twenty-Five: Implement GenericDAO abstract class

--- src/main/java/com/bookstore/dao/GenericDAO.java
+++ src/main/java/com/bookstore/dao/GenericDAO.java
@@ -1,5 +1,19 @@
 package com.bookstore.dao;
 
-public interface GenericDAO {
+import java.util.List;
+
+public interface GenericDAO<T> {
+	
+	public T create(T t);
+	
+	public T update(T t);
+	
+	public T get(Object id);
+	
+	public void delete(Object id);
+	
+	public List<T> listAll();
+	
+	public long count();
 
 }
