fccd14091508855bff5d531fbb33ec1f5f3b8f7c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-05-17 16:33:41 +0100

    Twenty-Nine Commit: Implement UserDAO class

--- src/main/java/com/bookstore/dao/UserDAO.java
+++ src/main/java/com/bookstore/dao/UserDAO.java
@@ -1,5 +1,52 @@
 package com.bookstore.dao;
 
-public class UserDAO extends JpaDAO<T> implements GenericDAO<T> {
+import java.util.List;
+
+import com.bookstore.entity.Users;
+
+import jakarta.persistence.EntityManager;
+
+public class UserDAO extends JpaDAO<Users> implements GenericDAO<Users> {
+
+	public UserDAO(EntityManager entityManager) {
+		super(entityManager);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Users create(Users user) {
+		// TODO Auto-generated method stub
+		return super.create(user);
+	}
+
+	@Override
+	public Users update(Users t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Users get(Object id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Object id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Users> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
