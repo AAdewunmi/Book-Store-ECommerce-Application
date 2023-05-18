68fd2d02521a43320c306a9903a578d13459d2cb
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-05-18 11:53:52 +0100

    Thirty-One Commit:Implement UserDAOTest class

--- test/com/bookstore/dao/UserDAOTest.java
+++ test/com/bookstore/dao/UserDAOTest.java
@@ -4,11 +4,33 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.bookstore.entity.Users;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class UserDAOTest {
 
 	@Test
 	public void testCreateUsers() {
-		fail("Not yet implemented");
+		Users user1 = new Users();
+		user1.setEmail("james.dyson@dyson.com");
+		user1.setFullName("Jame Dyson");
+		user1.setPassword("vacuumvacuum");
+		
+		EntityManagerFactory entityManagerFactory = 
+		Persistence.createEntityManagerFactory("Book-Store-ECommerce-Application");
+		
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		UserDAO userDAO = new UserDAO(entityManager);
+		user1 = userDAO.create(user1);
+		
+		entityManager.close();
+		entityManagerFactory.close();
+		
+		assertTrue(user1.getUserId() > 0);
 	}
 
 }
