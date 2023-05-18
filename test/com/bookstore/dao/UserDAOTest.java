b8948e5ff04e02b3d7800c00ec8ac029ae224845
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-05-18 11:08:14 +0100

    Thirty Commit: Create skeleton UserDAOTest class

---
+++ test/com/bookstore/dao/UserDAOTest.java
@@ -0,0 +1,14 @@
+package com.bookstore.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserDAOTest {
+
+	@Test
+	public void testCreateUsers() {
+		fail("Not yet implemented");
+	}
+
+}
