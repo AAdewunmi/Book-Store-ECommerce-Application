ce40dbf7433f92d1c88c3de769b0f766cf4025eb
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-05-27 13:51:43 +0100

    Sixty-Three Commit: Implement UserDAO get() method

--- src/main/java/com/bookstore/dao/UserDAO.java
+++ src/main/java/com/bookstore/dao/UserDAO.java
@@ -26,9 +26,9 @@ public class UserDAO extends JpaDAO<Users> implements GenericDAO<Users> {
 	}
 
 	@Override
-	public Users get(Object id) {
+	public Users get(Object userId) {
 		// TODO Auto-generated method stub
-		return null;
+		return super.find(Users.class, userId);
 	}
 
 	@Override
