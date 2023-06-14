2291b511de5dc818c870a4c073bba1f9a8352e47
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-06-14 15:22:25 +0100

    Eight-Five Commit: Create skeleton CreateUserServlet class

---
+++ src/main/java/com/bookstore/controller/admin/CreateUserServlet.java
@@ -0,0 +1,20 @@
+package com.bookstore.controller.admin;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/admin/create_user")
+public class CreateUserServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
