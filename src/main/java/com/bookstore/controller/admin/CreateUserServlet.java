0f06e836fa59d0f30e00d839a30e94e7faa9ea19
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-06-15 12:17:37 +0100

    Eighty-Seven Commit: Implement functionality to save user to database

--- src/main/java/com/bookstore/controller/admin/CreateUserServlet.java
+++ src/main/java/com/bookstore/controller/admin/CreateUserServlet.java
@@ -1,11 +1,14 @@
 package com.bookstore.controller.admin;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import com.bookstore.service.UserServices;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @WebServlet("/admin/create_user")
@@ -13,8 +16,24 @@ public class CreateUserServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
+	public CreateUserServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		response.getWriter().println("Email: " + email);
 		
+		String password = request.getParameter("password");
+		response.getWriter().println("Password: " + password);
+		
+		String fullname = request.getParameter("fullname");
+		response.getWriter().println("Fullname: " + fullname);
+		
+		UserServices userServices = new UserServices();
+		userServices.createUser(email, password, fullname);
+				
 	}
 
 }
