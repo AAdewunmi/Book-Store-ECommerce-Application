c03d1ad29e2bc5197d6e251dfc31a42740f37e59
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-06-17 13:35:24 +0100

    Ninety-Six Commit: Create skeleton EditUserServlet class

---
+++ src/main/java/com/bookstore/controller/admin/EditUserServlet.java
@@ -0,0 +1,29 @@
+package com.bookstore.controller.admin;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/edit_user")
+public class EditUserServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+       
+    public EditUserServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
