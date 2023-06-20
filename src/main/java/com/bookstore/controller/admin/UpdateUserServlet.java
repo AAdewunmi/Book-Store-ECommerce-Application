37f4071724d89b7192c44f6eac9307e67038d078
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-06-20 16:34:16 +0100

    One Hundred-Three Commit: Implement UserServices.updateUser() method

--- src/main/java/com/bookstore/controller/admin/UpdateUserServlet.java
+++ src/main/java/com/bookstore/controller/admin/UpdateUserServlet.java
@@ -1,11 +1,14 @@
 package com.bookstore.controller.admin;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import com.bookstore.service.UserServices;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/admin/update_user")
 public class UpdateUserServlet extends HttpServlet {
@@ -14,17 +17,11 @@ public class UpdateUserServlet extends HttpServlet {
        
     public UpdateUserServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		UserServices userServices = new UserServices(request, response);
+		userServices.updateUser();
 	}
 
 }
