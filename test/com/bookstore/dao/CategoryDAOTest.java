6cd7fbc6db749d5f00035671388fa7ed7eed0e0f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-06-26 14:42:22 +0100

    One Hundred-Seventeen Commit: Implement CategoryDAOTest class

---
+++ test/com/bookstore/dao/CategoryDAOTest.java
@@ -0,0 +1,60 @@
+package com.bookstore.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.bookstore.entity.Category;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class CategoryDAOTest {
+	private static EntityManagerFactory entityManagerFactory;
+	private static EntityManager entityManager;
+	private static CategoryDAO categoryDAO;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		
+	}
+
+	@Test
+	public void testCreateCategory() {
+		
+	}
+
+	@Test
+	public void testUpdateCategory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCount() {
+		fail("Not yet implemented");
+	}
+
+}
