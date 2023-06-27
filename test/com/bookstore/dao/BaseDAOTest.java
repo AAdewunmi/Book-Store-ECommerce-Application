cb57e447b777444ef255dc72e7119b5ec3f3090d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-06-27 09:30:31 +0100

    One Hundred-Twenty-Two Commit: Implement super class for DAO test classes

--- test/com/bookstore/dao/BaseDAOTest.java
+++ test/com/bookstore/dao/BaseDAOTest.java
@@ -1,5 +1,22 @@
 package com.bookstore.dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class BaseDAOTest {
+	
+	protected static EntityManagerFactory entityManagerFactory;
+	protected static EntityManager entityManager;
+	
+	public static void setUpBeforeClass() throws Exception {
+		entityManagerFactory = Persistence.createEntityManagerFactory("Book-Store-ECommerce-Application");
+		entityManager = entityManagerFactory.createEntityManager();
+	}
+	
+	public static void tearDownAfterClass() throws Exception {
+		entityManager.clear();
+		entityManagerFactory.close();
+	}
 
 }
