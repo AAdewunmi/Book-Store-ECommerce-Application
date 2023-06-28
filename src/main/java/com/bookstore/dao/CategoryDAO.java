c2e810a76eeabaa7833d6c2a6a7c4251e622c891
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-06-28 10:35:54 +0100

    One Hundred-Twenty-Nine Commit: Implement CategoryDAO.delete() method

--- src/main/java/com/bookstore/dao/CategoryDAO.java
+++ src/main/java/com/bookstore/dao/CategoryDAO.java
@@ -10,31 +10,26 @@ public class CategoryDAO extends JpaDAO<Category> implements GenericDAO<Category
 
 	public CategoryDAO(EntityManager entityManager) {
 		super(entityManager);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public Category create(Category category) {
-		// TODO Auto-generated method stub
 		return super.create(category);
 	}
 
 	@Override
 	public Category update(Category category) {
-		// TODO Auto-generated method stub
 		return super.update(category);
 	}
 
 	@Override
 	public Category get(Object id) {
-		// TODO Auto-generated method stub
 		return super.find(Category.class, id);
 	}
 
 	@Override
 	public void delete(Object id) {
-		// TODO Auto-generated method stub
-		
+		super.delete(Category.class, id);	
 	}
 
 	@Override
