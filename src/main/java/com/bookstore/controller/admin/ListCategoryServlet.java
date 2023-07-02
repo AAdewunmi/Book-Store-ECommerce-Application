b702237f8547bebb6da265d6152c172406b3e2ba
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-07-02 10:48:27 +0100

    One Hundred-Thirty-Seven Commit: Create skeleton ListCategoryServlet class

---
+++ src/main/java/com/bookstore/controller/admin/ListCategoryServlet.java
@@ -0,0 +1,26 @@
+package com.bookstore.controller.admin;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/list_category")
+public class ListCategoryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public ListCategoryServlet() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
