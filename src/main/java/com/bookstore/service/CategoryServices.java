ae6884178a10282c26803a7d2cdc94a403c204c8
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-07-15 17:30:23 +0100

    One-Hundred-Forty-Eight Commit: Implement List Category Feature

--- src/main/java/com/bookstore/service/CategoryServices.java
+++ src/main/java/com/bookstore/service/CategoryServices.java
@@ -1,5 +1,40 @@
 package com.bookstore.service;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.bookstore.dao.CategoryDAO;
+import com.bookstore.entity.Category;
+
+import jakarta.persistence.EntityManager;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class CategoryServices {
+	private EntityManager entityManager;
+	private CategoryDAO categoryDAO;
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	
+	public CategoryServices(EntityManager entityManager,
+			HttpServletRequest request, HttpServletResponse response) {
+		super();
+		this.request = request;
+		this.response = response;
+		this.entityManager = entityManager;
+		categoryDAO = new CategoryDAO(entityManager);
+	}
+
+	public void listCategory() throws ServletException, IOException {
+		List<Category> listCategory = categoryDAO.listAll();
+		request.setAttribute("listCategory", listCategory);
+		String listPage = "category_list.jsp";
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher(listPage);
+		requestDispatcher.forward(request, response);
+	}
+	
+	
 
 }
