fd694e49bbb8f9d8ce9779994b9410cc933cad88
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-07-21 11:30:31 +0100

    One-Hundred-Fifty-Two Commit: Create Hibernate NamedQuery statement

--- src/main/java/com/bookstore/entity/Category.java
+++ src/main/java/com/bookstore/entity/Category.java
@@ -23,7 +23,8 @@ import jakarta.persistence.*;
 @Table(name = "category", catalog = "bookstoredb")
 @NamedQueries({
 	@NamedQuery(name = "Category.findAll", query = "SELECT c FROM Category c ORDER BY c.name"),
-	@NamedQuery(name = "Category.countAll", query = "SELECT COUNT (*) FROM Category")
+	@NamedQuery(name = "Category.countAll", query = "SELECT COUNT (*) FROM Category"),
+	@NamedQuery(name = "Category.findByName", query = "SELECT c FROM Category c WHERE c.name = :name")
 })
 public class Category implements java.io.Serializable {
 
