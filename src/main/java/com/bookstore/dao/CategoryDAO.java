7e2b27a599bf798be16b17c3972ff9dca5cefcf8
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-07-21 14:30:45 +0100

    One-Hundred-Fifty-Five Commit: Implement findByName() method

--- src/main/java/com/bookstore/dao/CategoryDAO.java
+++ src/main/java/com/bookstore/dao/CategoryDAO.java
@@ -41,5 +41,12 @@ public class CategoryDAO extends JpaDAO<Category> implements GenericDAO<Category
 	public long count() {
 		return super.countWithNamedQuery("Category.countAll");
 	}
-
+    
+	public Category findByName(String categoryName) {
+		List<Category> result = super.findWithNamedQuery("Category.findByName", "name", categoryName);
+		if (result != null && result.size() > 0) {
+			return result.get(0);
+		}
+		return null;
+	}
 }
