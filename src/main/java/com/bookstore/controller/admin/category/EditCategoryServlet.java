1c9188a36a108172c54083df2fc17dd4f51a1896
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-07-23 11:38:56 +0100

    One-Hundred-Sixty Commit: Create skeleton EditCategoryServlet class

---
+++ src/main/java/com/bookstore/controller/admin/category/EditCategoryServlet.java
@@ -0,0 +1,25 @@
+package com.bookstore.controller.admin.category;
+
+import com.bookstore.controller.admin.BaseServlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/edit_category")
+
+public class EditCategoryServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+
+    
+    public EditCategoryServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
