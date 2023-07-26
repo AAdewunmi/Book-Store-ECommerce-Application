24f0288b528059a7e8307f4d4eac5e2f6e5bdc02
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-07-26 12:07:15 +0100

    One-Hundred-Sixty-Eight Commit: Create skeleton DeleteCategoryServlet class

---
+++ src/main/java/com/bookstore/controller/admin/category/DeleteCategoryServlet.java
@@ -0,0 +1,26 @@
+package com.bookstore.controller.admin.category;
+
+import com.bookstore.controller.admin.BaseServlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet("/admin/delete_category")
+public class DeleteCategoryServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+
+    public DeleteCategoryServlet() {
+        
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
