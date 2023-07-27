bf16dadb672cf78838d799f284a426109c111aaa
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-07-27 10:03:27 +0100

    One Hundred-Seventy-One Commit: Refactor HomeServlet class

--- src/main/java/com/bookstore/controller/frontend/HomeServlet.java
+++ src/main/java/com/bookstore/controller/frontend/HomeServlet.java
@@ -1,23 +1,23 @@
 package com.bookstore.controller.frontend;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.RequestDispatcher;
+import com.bookstore.controller.admin.BaseServlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
 
 @WebServlet("")
-public class HomeServlet extends HttpServlet {
+public class HomeServlet extends BaseServlet {
 	private static final long serialVersionUID = 1L;
        
     
     public HomeServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	
