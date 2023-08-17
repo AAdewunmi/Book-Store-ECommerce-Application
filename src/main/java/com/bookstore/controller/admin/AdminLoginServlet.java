6d91858d0fef8fd9ee97ef42ba06618774d92462
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-08-17 08:16:24 +0100

    One-Hundred-Four Commit: Create skeleton AdminLoginServlet class

---
+++ src/main/java/com/bookstore/controller/admin/AdminLoginServlet.java
@@ -0,0 +1,26 @@
+package com.bookstore.controller.admin;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+
+@WebServlet("/login")
+public class AdminLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public AdminLoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
