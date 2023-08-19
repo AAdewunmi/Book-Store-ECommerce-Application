900494a411f9ef03eb6808d652d9941e4e51fe0d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-08-19 07:45:17 +0100

    Two-Hundred-Eleven Commit: Implement logout functionality for administrator

--- src/main/java/com/bookstore/controller/admin/AdminLogoutServlet.java
+++ src/main/java/com/bookstore/controller/admin/AdminLogoutServlet.java
@@ -1,10 +1,12 @@
 package com.bookstore.controller.admin;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -17,7 +19,10 @@ public class AdminLogoutServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		session.removeAttribute("useremail");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
