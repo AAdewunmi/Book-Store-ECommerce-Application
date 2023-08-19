fe7738c44e49d2516e8efa2f4ef6fbcbf1752ca8
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-08-19 07:33:16 +0100

    One-Hundred-Ten Commit: Create skeleton AdminLogoutServlet servlet

---
+++ src/main/java/com/bookstore/controller/admin/AdminLogoutServlet.java
@@ -0,0 +1,27 @@
+package com.bookstore.controller.admin;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/admin/logout")
+public class AdminLogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public AdminLogoutServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
