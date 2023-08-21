803c2e2e5ce687ba46611b9693f30ca3b4b75cba
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-08-21 11:31:54 +0100

    Two-Hundred-Seventeen Commit: Implement HashGenerationException class

--- src/main/java/com/bookstore/dao/HashGenerationException.java
+++ src/main/java/com/bookstore/dao/HashGenerationException.java
@@ -1,5 +1,13 @@
 package com.bookstore.dao;
 
 public class HashGenerationException {
+	
+	public HashGenerationException(String message) {
+		super(message);
+	}
+
+	public HashGenerationException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
 }
