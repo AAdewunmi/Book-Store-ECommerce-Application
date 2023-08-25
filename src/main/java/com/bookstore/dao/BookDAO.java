4f5acbaa8d6a7debb29757abb04395e732c78ccf
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-08-25 07:54:21 +0100

    Two-Hundred-Thirty Commit: Implement create() method in BookDAO class

--- src/main/java/com/bookstore/dao/BookDAO.java
+++ src/main/java/com/bookstore/dao/BookDAO.java
@@ -1,5 +1,53 @@
 package com.bookstore.dao;
 
-public class BookDAO extends JpaDAO<E> implements GenericDAO<E> {
+import java.util.Date;
+import java.util.List;
+
+import com.bookstore.entity.Book;
+
+import jakarta.persistence.EntityManager;
+
+public class BookDAO extends JpaDAO<Book> implements GenericDAO<Book> {
+
+	public BookDAO(EntityManager entityManager) {
+		super(entityManager);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Book create(Book book) {
+		book.setLastUpdateTime(new Date());
+		return super.create(book);
+	}
+
+	@Override
+	public Book update(Book book) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Book get(Object id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Object id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Book> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
