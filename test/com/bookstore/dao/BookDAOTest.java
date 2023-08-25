0716bfd04000370caea0c2515a29653ae4245acd
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-08-25 08:01:04 +0100

    Two-Hundred-Thirty-One Commit: Create skeleton BookDAOTest class

---
+++ test/com/bookstore/dao/BookDAOTest.java
@@ -0,0 +1,24 @@
+package com.bookstore.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BookDAOTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testCreateBook() {
+		fail("Not yet implemented");
+	}
+
+}
