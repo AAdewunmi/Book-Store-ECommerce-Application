395b24033d207b05786003b1f11f6de71bce9148
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-08-26 12:16:16 +0100

    Two-Hundred-Thirty-Three Commit: Implement testCreateBook() method

--- test/com/bookstore/dao/BookDAOTest.java
+++ test/com/bookstore/dao/BookDAOTest.java
@@ -2,9 +2,19 @@ package com.bookstore.dao;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.bookstore.entity.Book;
+import com.bookstore.entity.Category;
 
 public class BookDAOTest extends BaseDAOTest{
 	private static BookDAO bookDAO;
@@ -21,8 +31,24 @@ public class BookDAOTest extends BaseDAOTest{
 	}
 
 	@Test
-	public void testCreateBook() {
-		fail("Not yet implemented");
-	}
+	public void testCreateBook() throws ParseException, IOException {
+		Book book=new Book();
+		Category category=new Category("Advanced Java");
+		category.setCategoryId(5);
+		book.setCategory(category);
+		book.setTitle("Effective Java (2nd Edition)");
+		book.setAuthor("Joshua Bloch");
+		book.setDescription("New coverage of generics, enums, annotations, autoboxing");
+		book.setPrice(38.87f);
+		book.setIsbn("0321356683");
+		SimpleDateFormat dateFormat=new SimpleDateFormat("MM/dd/yyyy");
+		Date publishDate=dateFormat.parse("05/28/2008");
+		book.setPublishDate(publishDate);
+		String imagePath="/Users/adrianadewunmi/Downloads/books/Effective Java.jpg";
+		byte[] imageBytes=Files.readAllBytes(Paths.get(imagePath));
+		book.setImage(imageBytes);
+		Book createdBook =bookDAO.create(book);
+		assertTrue(createdBook.getBookId()>0);	
+		}
 
 }
