47b059b4d1cae123abe794f88dfdc793a26f5213
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-09-06 09:46:30 +0100

    Two-Hundred-Fifty-Nine Commit: Implement constructor for BookServices class

--- src/main/java/com/bookstore/service/BookServices.java
+++ src/main/java/com/bookstore/service/BookServices.java
@@ -1,5 +1,28 @@
 package com.bookstore.service;
 
+import com.bookstore.dao.BookDAO;
+
+import jakarta.persistence.EntityManager;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class BookServices {
+	
+	private EntityManager entityManager;
+	private BookDAO bookDAO;
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	
+	public BookServices(EntityManager entityManager, HttpServletRequest request, HttpServletResponse response) {
+		super();
+		this.entityManager = entityManager;
+		this.request = request;
+		this.response = response;
+		bookDAO = new BookDAO(entityManager);
+	}
+
+	public void listBooks() {
+		
+	}
 
 }
