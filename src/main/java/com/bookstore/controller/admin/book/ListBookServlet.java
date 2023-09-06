5698ecad62f48a8903adfc9f2e50387f401aad3d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-09-06 09:34:06 +0100

    Two-Hundred-Fifty-Eight Commit: Create skeleton BookServices class

--- src/main/java/com/bookstore/controller/admin/book/ListBookServlet.java
+++ src/main/java/com/bookstore/controller/admin/book/ListBookServlet.java
@@ -1,40 +1,27 @@
 package com.bookstore.controller.admin.book;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ListBookServlet
- */
+import com.bookstore.controller.BaseServlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @WebServlet("/admin/list_books")
-public class ListBookServlet extends HttpServlet {
+public class ListBookServlet extends BaseServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ListBookServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
