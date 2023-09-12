247ad9cd35dc6aad792d8f33ba3358e72aea8766
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-09-12 08:03:18 +0100

    Two-Hundred-Sixty-Eight Commit: Create skeleton NewBookServlet class

---
+++ src/main/java/com/bookstore/controller/admin/book/NewBookServlet.java
@@ -0,0 +1,31 @@
+package com.bookstore.controller.admin.book;
+
+import com.bookstore.controller.BaseServlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet("/admin/new_book")
+public class NewBookServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+
+    public NewBookServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
