46b11bebcf4cf2ec288f0ed4e2d616e55d59b8de
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-09-28 10:26:24 +0100

    Two-Hundred-Ninety-Two Commit: Implement doPost() method in UpdateBookServlet class

--- src/main/java/com/bookstore/controller/admin/book/UpdateBookServlet.java
+++ src/main/java/com/bookstore/controller/admin/book/UpdateBookServlet.java
@@ -1,8 +1,10 @@
 package com.bookstore.controller.admin.book;
 
 import com.bookstore.controller.BaseServlet;
+import com.bookstore.service.BookServices;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,6 +12,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/admin/update_book")
+@MultipartConfig(
+		fileSizeThreshold = 1024*10, //10 KB
+		maxFileSize = 1024*300,   	//300 KB
+		maxRequestSize = 1024*1024 	//1 MB
+		)
 public class UpdateBookServlet extends BaseServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -18,7 +25,8 @@ public class UpdateBookServlet extends BaseServlet {
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		BookServices bookServices = new BookServices(entityManager, request, response);
+		bookServices.updateBook();
 	}
 
 }
