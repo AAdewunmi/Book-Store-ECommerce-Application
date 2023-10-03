756414913a62b305e9bf475566ca0793e3de9fd5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-03 16:31:38 +0100

    Three-Hundred-One Commit: Implement DeleteBookServlet class

--- src/main/java/com/bookstore/controller/admin/book/DeleteBookServlet.java
+++ src/main/java/com/bookstore/controller/admin/book/DeleteBookServlet.java
@@ -1,23 +1,24 @@
 package com.bookstore.controller.admin.book;
 
 import com.bookstore.controller.BaseServlet;
+import com.bookstore.service.BookServices;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 @WebServlet("/admin/delete_book")
 public class DeleteBookServlet extends BaseServlet {
 	private static final long serialVersionUID = 1L;
 
-    public DeleteBookServlet() {
-        
-    }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		BookServices bookServices = new BookServices(entityManager, request, response);
+		bookServices.deleteBook();
 	}
 
 }
