7e0ef91e799a70861b8f87dcf80d091a7e4ad1b2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-03 16:14:52 +0100

    Three-Hundred Commit: Create skeleton DeleteBookServlet class

---
+++ src/main/java/com/bookstore/controller/admin/book/DeleteBookServlet.java
@@ -0,0 +1,23 @@
+package com.bookstore.controller.admin.book;
+
+import com.bookstore.controller.BaseServlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/delete_book")
+public class DeleteBookServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+
+    public DeleteBookServlet() {
+        
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
