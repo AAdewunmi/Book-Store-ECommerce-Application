a6a097a3f7289f64284fefefb105f28546005f79
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-24 15:12:43 +0100

    Three-Hundred-Twenty-Six Commit: Implement ViewBookServlet class

--- src/main/java/com/bookstore/controller/frontend/ViewBookServlet.java
+++ src/main/java/com/bookstore/controller/frontend/ViewBookServlet.java
@@ -1,6 +1,8 @@
 package com.bookstore.controller.frontend;
 
 import com.bookstore.controller.BaseServlet;
+import com.bookstore.service.BookServices;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,13 +14,11 @@ import java.io.IOException;
 public class ViewBookServlet extends BaseServlet {
 	private static final long serialVersionUID = 1L;
 
-    public ViewBookServlet() {
-        // TODO Auto-generated constructor stub
-    }
+    public ViewBookServlet() {}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		BookServices bookServices = new BookServices(entityManager, request, response);
+		bookServices.viewBookDetail();
 	}
 
 }
