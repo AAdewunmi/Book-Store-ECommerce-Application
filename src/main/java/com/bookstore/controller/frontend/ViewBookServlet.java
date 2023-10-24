609293eb78c8bd9e6d68533002c8fff8a33a4fa6
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-24 10:15:17 +0100

    Three-Hundred-Twenty-Four Commit: Create skeleton ViewBookServlet class

---
+++ src/main/java/com/bookstore/controller/frontend/ViewBookServlet.java
@@ -0,0 +1,24 @@
+package com.bookstore.controller.frontend;
+
+import com.bookstore.controller.BaseServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/view_book")
+public class ViewBookServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ViewBookServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
