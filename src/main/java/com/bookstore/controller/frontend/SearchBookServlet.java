9b92671ce6a56af7e6618ca2249804371c6feb25
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-27 17:03:55 +0100

    Three-Hundred-Thirty-Six Commit: Create skeleton SearchBookServlet class

---
+++ src/main/java/com/bookstore/controller/frontend/SearchBookServlet.java
@@ -0,0 +1,27 @@
+package com.bookstore.controller.frontend;
+
+import java.io.IOException;
+
+import com.bookstore.controller.BaseServlet;
+import com.bookstore.service.BookServices;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/search")
+public class SearchBookServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+
+    public SearchBookServlet() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//BookServices bookServices = new BookServices(entityManager, request, response);
+		//bookServices.listBooksByCategory();
+    	super.doGet(request, response);
+	}
+
+}
