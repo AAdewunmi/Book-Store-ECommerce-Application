233362922f85e85bc3856f85eb4921cb95a9939e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-03 13:07:49 +0000

    Three-Hundred-Sixty Commit: Refactor AdminLoginServlet class to delete usage of BaseServlet class and EntityManagerFactory class

--- src/main/java/com/bookstore/controller/admin/AdminLoginServlet.java
+++ src/main/java/com/bookstore/controller/admin/AdminLoginServlet.java
@@ -6,21 +6,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-
-import com.bookstore.controller.BaseServlet;
 import com.bookstore.service.UserServices;
 
 @WebServlet("/admin/login")
-public class AdminLoginServlet extends BaseServlet {
+public class AdminLoginServlet{
 	private static final long serialVersionUID = 1L;
        
     public AdminLoginServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		UserServices userServices = new UserServices(entityManager, request, response);
+		UserServices userServices = new UserServices(request, response);
 		userServices.login();
 		
 	}
