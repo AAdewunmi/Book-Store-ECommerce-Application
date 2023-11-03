217e8045e0e535b55186cad6334295dca8e01fe2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-03 13:17:22 +0000

    Three-Hundred-Sixty-Two Commit: Refactor DeleteBookServlet class to delete usage of BaseServlet class and EntityManager class

--- src/main/java/com/bookstore/controller/admin/book/DeleteBookServlet.java
+++ src/main/java/com/bookstore/controller/admin/book/DeleteBookServlet.java
@@ -5,6 +5,7 @@ import com.bookstore.service.BookServices;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -12,12 +13,12 @@ import java.io.IOException;
 
 
 @WebServlet("/admin/delete_book")
-public class DeleteBookServlet extends BaseServlet {
+public class DeleteBookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		BookServices bookServices = new BookServices(entityManager, request, response);
+		BookServices bookServices = new BookServices(request, response);
 		bookServices.deleteBook();
 	}
 
