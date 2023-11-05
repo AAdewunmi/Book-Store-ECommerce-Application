145dbb476ed62ac7d6be78d56d916fa13a1646fc
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-05 11:29:17 +0000

    Three-Hundred-Seventy-Two Commit: Implement CommonFilter class

--- src/main/java/com/bookstore/controller/frontend/CommonFilter.java
+++ src/main/java/com/bookstore/controller/frontend/CommonFilter.java
@@ -8,15 +8,24 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
 import java.io.IOException;
+import java.util.List;
+
+import com.bookstore.dao.CategoryDAO;
+import com.bookstore.entity.Category;
 
 @WebFilter("/*")
 public class CommonFilter extends HttpFilter implements Filter {
        
     private static final long serialVersionUID = 1L;
-
+    
+    private final CategoryDAO categoryDAO;
+    
 	public CommonFilter() {
         super();
+        this.categoryDAO = new CategoryDAO();
     }
 
 	public void destroy() {
@@ -24,7 +33,13 @@ public class CommonFilter extends HttpFilter implements Filter {
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		
+		HttpServletRequest httpRequest =  (HttpServletRequest) request;
+		String path = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length());
+		if (!path.startsWith("/admin/")) {
+			List<Category> listCategory = categoryDAO.listAll();
+			request.setAttribute("listCategory", listCategory);
+			
+		}
 		chain.doFilter(request, response);
 	}
 
