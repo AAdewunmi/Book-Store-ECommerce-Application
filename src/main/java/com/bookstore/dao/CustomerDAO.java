c9c3ebec71ff3e324509391d8aed8f512cf271d8
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-14 13:43:43 +0000

    Three-Hundred-Ninety-Five Commit: Implement create() method in CustomerDAO class

--- src/main/java/com/bookstore/dao/CustomerDAO.java
+++ src/main/java/com/bookstore/dao/CustomerDAO.java
@@ -1,5 +1,46 @@
 package com.bookstore.dao;
 
-public class CustomerDAO extends JpaDAO<E> implements GenericDAO<E> {
+import java.util.Date;
+import java.util.List;
+
+import com.bookstore.entity.Customer;
+
+public class CustomerDAO extends JpaDAO<Customer> implements GenericDAO<Customer> {
+
+	@Override
+	public Customer create(Customer customer) {
+		customer.setRegisterDate(new Date());
+		return super.create(customer);
+	}
+
+	@Override
+	public Customer update(Customer t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer get(Object id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Object id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Customer> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
