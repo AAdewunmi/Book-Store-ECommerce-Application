0cdad0ce0d159b29ebb19e9714f96815f1c817b8
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-14 13:56:43 +0000

    Three-Hundred-Ninety-Eight Commit: Create skeleton CustomerDAOTest class

---
+++ test/com/bookstore/dao/CustomerDAOTest.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.bookstore.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class CustomerDAOTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.bookstore.dao.CustomerDAO#create(com.bookstore.entity.Customer)}.
+	 */
+	@Test
+	public void testCreateCustomer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.bookstore.dao.CustomerDAO#get(java.lang.Object)}.
+	 */
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.bookstore.dao.CustomerDAO#delete(java.lang.Object)}.
+	 */
+	@Test
+	public void testDeleteObject() {
+		fail("Not yet implemented");
+	}
+
+}
