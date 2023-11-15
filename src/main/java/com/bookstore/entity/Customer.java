27c3eb27345923ed824e50a8338377cc1ff5f2d5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-15 08:35:41 +0000

    Four-Hundred-Seven Commit: Implement @NamedQuery Customer.countAll in Customer class

--- src/main/java/com/bookstore/entity/Customer.java
+++ src/main/java/com/bookstore/entity/Customer.java
@@ -14,6 +14,7 @@ import jakarta.persistence.*;
 @Table(name = "customer", catalog = "bookstoredb", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 @NamedQueries({
 	@NamedQuery(name = "Customer.findAll", query = "SELECT c FROM Customer c ORDER BY c.registerDate DESC"),
+	@NamedQuery(name = "Customer.countAll", query = "SELECT COUNT(c.email) FROM Customer c"),
 })
 public class Customer implements java.io.Serializable {
 
