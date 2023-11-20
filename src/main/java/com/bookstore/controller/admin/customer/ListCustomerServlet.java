4b2e1bcbcaec5815f8d4dd7f82314325480d82fe
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-20 16:41:12 +0000

    Four-Hundred-Ten Commit: Create skeleton ListCustomerServlet class

---
+++ src/main/java/com/bookstore/controller/admin/customer/ListCustomerServlet.java
@@ -0,0 +1,22 @@
+package com.bookstore.controller.admin.customer;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/list_customer")
+public class ListCustomerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ListCustomerServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
