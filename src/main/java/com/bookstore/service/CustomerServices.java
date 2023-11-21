fcdb4bce3405c08a752fd28e64f6f9641fbc4e70
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-21 10:48:52 +0000

    Four-Hundred-Thirteen Commit: Implement listCustomer() method in CustomerServices class

--- src/main/java/com/bookstore/service/CustomerServices.java
+++ src/main/java/com/bookstore/service/CustomerServices.java
@@ -1,5 +1,34 @@
 package com.bookstore.service;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.bookstore.dao.CustomerDAO;
+import com.bookstore.entity.Customer;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class CustomerServices {
+	
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	
+	public CustomerServices(HttpServletRequest request, HttpServletResponse response) {
+		super();
+		this.request = request;
+		this.response = response;
+	}
+	
+	public void listCustomers() throws ServletException, IOException {
+		CustomerDAO customerDAO = new CustomerDAO();
+		List<Customer> listCustomer = customerDAO.listAll();
+		request.setAttribute("listCustomer", listCustomer);
+		String listPage = "customer_list.jsp";
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("listPage");
+		requestDispatcher.forward(request, response);
+	}
 
 }
