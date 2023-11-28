0f9bcf1470a145e00ac8a2a357485d5a88a9184e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-28 14:20:33 +0000

    Four-Hundred-Twenty-Three Commit: Implement doPost() method in CreateCustomerServlet class

--- src/main/java/com/bookstore/controller/admin/customer/CreateCustomerServlet.java
+++ src/main/java/com/bookstore/controller/admin/customer/CreateCustomerServlet.java
@@ -1,11 +1,15 @@
 package com.bookstore.controller.admin.customer;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+
+import com.bookstore.service.CustomerServices;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/admin/create_customer")
 public class CreateCustomerServlet extends HttpServlet {
@@ -18,7 +22,8 @@ public class CreateCustomerServlet extends HttpServlet {
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		CustomerServices customerServices = new CustomerServices(request, response);
+		customerServices.createCustomer();
 	}
 
 }
