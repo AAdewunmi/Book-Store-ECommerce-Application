f7bd77ddfe92d7ef0a3651e6c49fdeca63d3d6a2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-04 11:00:38 +0000

    Four-Hundred-Twenty-Seven Commit: Refactor listCustomers() method

--- src/main/java/com/bookstore/service/CustomerServices.java
+++ src/main/java/com/bookstore/service/CustomerServices.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class CustomerServices {
-	
+	private CustomerDAO customerDAO;
 	private HttpServletRequest request;
 	private HttpServletResponse response;
 	
@@ -20,15 +20,24 @@ public class CustomerServices {
 		super();
 		this.request = request;
 		this.response = response;
+		this.customerDAO = new CustomerDAO();
 	}
 	
-	public void listCustomers() throws ServletException, IOException {
-		CustomerDAO customerDAO = new CustomerDAO();
+	public void listCustomers(String message) throws ServletException, IOException {
+		//CustomerDAO customerDAO = new CustomerDAO();
 		List<Customer> listCustomer = customerDAO.listAll();
+		if (message != null) {
+			request.setAttribute("message", message);
+		}
 		request.setAttribute("listCustomer", listCustomer);
 		String listPage = "customer_list.jsp";
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher(listPage);
 		requestDispatcher.forward(request, response);
+	}
+	
+	public void createCustomer() {
+		String email = request.getParameter("email");
+		
 	}
 
 }
