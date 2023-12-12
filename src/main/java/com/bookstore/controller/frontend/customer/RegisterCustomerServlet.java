d2df523c98548f984cf324bcae23f2af55dca65f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-12 13:46:36 +0000

    Four-Hundred-Forty-Seven Commit: Create skeleton RegisterCustomerServlet class

---
+++ src/main/java/com/bookstore/controller/frontend/customer/RegisterCustomerServlet.java
@@ -0,0 +1,23 @@
+package com.bookstore.controller.frontend.customer;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/register_customer")
+public class RegisterCustomerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public RegisterCustomerServlet() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
