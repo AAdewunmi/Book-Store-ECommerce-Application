f5e2f63c7d6c38ab31df2b59231dbd44334097be
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-20 13:07:05 +0000

    Four-Hundred-Sixty-Five Commit: Implement checkLogin() method in CustomerDAO class

--- src/main/java/com/bookstore/dao/CustomerDAO.java
+++ src/main/java/com/bookstore/dao/CustomerDAO.java
@@ -1,9 +1,13 @@
 package com.bookstore.dao;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.bookstore.entity.Customer;
+
+import jakarta.servlet.RequestDispatcher;
 
 public class CustomerDAO extends JpaDAO<Customer> implements GenericDAO<Customer> {
 
@@ -40,6 +44,17 @@ public class CustomerDAO extends JpaDAO<Customer> implements GenericDAO<Customer
 	
 	public Customer findByEmail(String email) {
 		List<Customer> result = super.findWithNamedQuery("Customer.findByEmail", "email", email);
+		if (!result.isEmpty()) {
+			return result.get(0);
+		}
+		return null;
+	}
+	
+	public Customer checkLogin(String email, String password) {
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("email", email);
+		parameters.put("password", password);
+		List<Customer> result = super.findWithNamedQuery("Customer.checkLogin", parameters);
 		if (!result.isEmpty()) {
 			return result.get(0);
 		}
