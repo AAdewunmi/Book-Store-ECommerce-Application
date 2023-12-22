bb6ca84346b7634474a2ed20a7a90b17b4063438
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-22 14:26:46 +0000

    Four-Hundred-Seventy-Four Commit: Implement skeleton CustomerLoginFilter class

---
+++ src/main/java/com/bookstore/controller/frontend/CustomerLoginFilter.java
@@ -0,0 +1,32 @@
+package com.bookstore.controller.frontend;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class CustomerLoginFilter implements Filter {
+
+    public CustomerLoginFilter() {
+        
+    }
+
+	public void destroy() {
+		
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		
+	}
+
+}
