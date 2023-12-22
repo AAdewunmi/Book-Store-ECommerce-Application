a868d399ed09268c54af8b061a539b53ad287236
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-22 15:09:08 +0000

    Four-Hundred-Seventy-Seven Commit: Implement skeleton ShowCustomerProfileServlet class

---
+++ src/main/java/com/bookstore/controller/frontend/ShowCustomerProfileServlet.java
@@ -0,0 +1,23 @@
+package com.bookstore.controller.frontend;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/view_profile")
+public class ShowCustomerProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ShowCustomerProfileServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
