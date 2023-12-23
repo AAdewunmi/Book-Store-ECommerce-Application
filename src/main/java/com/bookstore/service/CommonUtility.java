c99eff02099d13ca144d9c64817ac00ba83462cb
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-23 11:58:28 +0000

    Four-Hundred-Eighty-One Commit: Implement CommonUtility class

--- src/main/java/com/bookstore/service/CommonUtility.java
+++ src/main/java/com/bookstore/service/CommonUtility.java
@@ -1,5 +1,36 @@
 package com.bookstore.service;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class CommonUtility {
+	
+	public static void forwardToPage(String page, String message,
+			HttpServletRequest request, HttpServletResponse response) 
+					throws ServletException, IOException {
+		request.setAttribute("message", message);
+		request.getRequestDispatcher(page).forward(request, response);		
+	}
+	
+	public static void showMessageFrontend(String message, 
+			HttpServletRequest request, HttpServletResponse response) 
+					throws ServletException, IOException {
+		forwardToPage("frontend/message.jsp", message, request, response);
+	}
+	
+	public static void showMessageBackend(String message, 
+			HttpServletRequest request, HttpServletResponse response) 
+					throws ServletException, IOException {
+		forwardToPage("message.jsp", message, request, response);
+	}
+
+	public static void forwardToPage(String page,
+			HttpServletRequest request, HttpServletResponse response) 
+					throws ServletException, IOException {
+		request.getRequestDispatcher(page).forward(request, response);		
+	}	
 
 }
