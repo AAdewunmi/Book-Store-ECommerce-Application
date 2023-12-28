0bfeea132b053121a15f3559c0217b9bdc3059b2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-28 11:00:19 +0000

    Four-Hundred-Eighty-Six Commit: Create skeleton EditCustomerProfileServlet class

---
+++ src/main/java/com/bookstore/controller/frontend/customer/EditCustomerProfileServlet.java
@@ -0,0 +1,24 @@
+package com.bookstore.controller.frontend.customer;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/edit_customer")
+public class EditCustomerProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public EditCustomerProfileServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
