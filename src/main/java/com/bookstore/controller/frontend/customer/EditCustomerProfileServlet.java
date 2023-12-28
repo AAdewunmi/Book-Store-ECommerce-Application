0c9c8df5ab2f200ee7aa682a659a717da6e2eb3a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-28 12:38:02 +0000

    Four-Hundred-Eighty-Eight Commit: Implement doGet() method in EditCustomerProfileServlet class

--- src/main/java/com/bookstore/controller/frontend/customer/EditCustomerProfileServlet.java
+++ src/main/java/com/bookstore/controller/frontend/customer/EditCustomerProfileServlet.java
@@ -2,6 +2,8 @@ package com.bookstore.controller.frontend.customer;
 
 import java.io.IOException;
 
+import com.bookstore.service.CustomerServices;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,7 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/edit_customer")
+@WebServlet("/edit_profile")
 public class EditCustomerProfileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -18,7 +20,8 @@ public class EditCustomerProfileServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		CustomerServices customerServices = new CustomerServices(request, response);
+		customerServices.showCustomerProfileEditForm();
 	}
 
 }
