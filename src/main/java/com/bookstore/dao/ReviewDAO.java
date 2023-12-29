222a76095c4282650f010494be5d5e1d3bc9e643
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-29 12:05:42 +0000

    Four-Hundred-Ninety-Five Commit: Implement create() method in ReviewDAO class

--- src/main/java/com/bookstore/dao/ReviewDAO.java
+++ src/main/java/com/bookstore/dao/ReviewDAO.java
@@ -1,5 +1,48 @@
 package com.bookstore.dao;
 
+import java.util.Date;
+import java.util.List;
+
+import com.bookstore.entity.Review;
+
 public class ReviewDAO extends JpaDAO<Review> implements GenericDAO<Review> {
+	
+	@Override
+	public Review create(Review review) {
+		review.setReviewTime(new Date());
+		return super.create(review);
+	}
+
+	@Override
+	public Review update(Review t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Review get(Object id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Object id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Review> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
 
 }
