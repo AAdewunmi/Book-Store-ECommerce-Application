5b833dd5d5cbdf67aac568a51c985bbaea9b2f8e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-29 12:10:25 +0000

    Four-Hundred-Ninety-Five Commit: Create skeleton ReviewDAOTest class

---
+++ test/com/bookstore/dao/ReviewDAOTest.java
@@ -0,0 +1,59 @@
+package com.bookstore.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ReviewDAOTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreateReview() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateReview() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCount() {
+		fail("Not yet implemented");
+	}
+
+}
