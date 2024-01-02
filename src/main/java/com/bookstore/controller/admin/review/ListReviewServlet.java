08930f6c3a09986159aecea9d0f99c8ba00d8f9c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-02 11:56:33 +0000

    Five-Hundred-Eleven Commit: Create skeleton ListReviewServlet class

---
+++ src/main/java/com/bookstore/controller/admin/review/ListReviewServlet.java
@@ -0,0 +1,23 @@
+package com.bookstore.controller.admin.review;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/list_review")
+public class ListReviewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ListReviewServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
