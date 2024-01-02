7ea21e2947bd3cd392d9a821a0f5758872ba6adc
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-02 14:30:38 +0000

    Four-Hundred-Sixteen Commit: Implement listAllReview() method in ReviewServices class

--- src/main/java/com/bookstore/service/ReviewServices.java
+++ src/main/java/com/bookstore/service/ReviewServices.java
@@ -1,5 +1,35 @@
 package com.bookstore.service;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.bookstore.dao.ReviewDAO;
+import com.bookstore.entity.Review;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class ReviewServices {
+	
+	private ReviewDAO reviewDAO;
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	
+	public ReviewServices(HttpServletRequest request, HttpServletResponse response) {
+		super();
+		this.request = request;
+		this.response = response;
+		this.reviewDAO = new ReviewDAO();
+	}
+	
+	public void listAllReview() throws ServletException, IOException {
+		List<Review> listReviews = reviewDAO.listAll();
+		request.setAttribute("listReviews", listReviews);
+		String listPage = "review_list.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(listPage);
+		dispatcher.forward(request, response);
+	}
 
 }
