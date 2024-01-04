af6a30aa81b63016c2c510b6762aebb08df8b79f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-04 16:41:06 +0000

    Five-Hundred-Twenty-Three Commit: Implement updateReview() method in ReviewServices class

--- src/main/java/com/bookstore/service/ReviewServices.java
+++ src/main/java/com/bookstore/service/ReviewServices.java
@@ -25,8 +25,15 @@ public class ReviewServices {
 	}
 	
 	public void listAllReview() throws ServletException, IOException {
+		listAllReview(null);
+	}
+	
+	public void listAllReview(String message) throws ServletException, IOException {
 		List<Review> listReviews = reviewDAO.listAll();
 		request.setAttribute("listReviews", listReviews);
+		if (message != null) {
+			request.setAttribute("message", message);
+		}
 		String listPage = "review_list.jsp";
 		RequestDispatcher dispatcher = request.getRequestDispatcher(listPage);
 		dispatcher.forward(request, response);
@@ -35,9 +42,22 @@ public class ReviewServices {
 	public void editReview() throws ServletException, IOException {
 		Integer reviewId = Integer.parseInt(request.getParameter("id"));
 		Review review = reviewDAO.get(reviewId);
+		request.setAttribute("review", review);
 		String editPage = "review_form.jsp";
 		RequestDispatcher dispatcher = request.getRequestDispatcher(editPage);
 		dispatcher.forward(request, response);
+	}
+
+	public void updateReview() throws ServletException, IOException {
+		Integer reviewId = Integer.parseInt(request.getParameter("reviewId"));
+		String headline = request.getParameter("headline");
+		String comment = request.getParameter("comment");
+		Review review = reviewDAO.get(reviewId);
+		review.setHeadline(headline);
+		review.setComment(comment);
+		reviewDAO.update(review);
+		String message = "The review has been updated successfully!";
+		listAllReview(message);
 	}
 
 }
