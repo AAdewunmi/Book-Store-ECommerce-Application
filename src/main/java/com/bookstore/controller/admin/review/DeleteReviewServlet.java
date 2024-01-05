fe5068751c998b1c4880aa761720ea4011d9528d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-05 14:05:55 +0000

    Five-Hundred-Twenty-Six Commit: Create skeleton DeleteReviewServlet class

---
+++ src/main/java/com/bookstore/controller/admin/review/DeleteReviewServlet.java
@@ -0,0 +1,23 @@
+package com.bookstore.controller.admin.review;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/delete_review")
+public class DeleteReviewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public DeleteReviewServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
