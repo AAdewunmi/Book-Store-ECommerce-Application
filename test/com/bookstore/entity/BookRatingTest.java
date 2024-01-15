4d42995b46013e39de8a09588fad7f60b405258d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-15 10:34:54 +0000

    Five-Hundred-Thirty-Five Commit: Create skeleton BookRatingTest class

---
+++ test/com/bookstore/entity/BookRatingTest.java
@@ -0,0 +1,13 @@
+package com.bookstore.entity;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookRatingTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- test/com/bookstore/entity/UsersTest.java
+++ test/com/bookstore/entity/UsersTest.java
@@ -1,3 +1,4 @@
+package com.bookstore.entity;
 
 import com.bookstore.entity.Users;
 
