4538233d6d147593eec1ca9ae1fe147ef31e6398
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-15 10:51:14 +0000

    Five-Hundred-Thirty-Six Commit: Implement testAverageRating1() method in BookRatingTest class

--- test/com/bookstore/entity/BookRatingTest.java
+++ test/com/bookstore/entity/BookRatingTest.java
@@ -1,13 +1,24 @@
 package com.bookstore.entity;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
 public class BookRatingTest {
 
+	@SuppressWarnings("deprecation")
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAverageRating1() {
+		Book book = new Book();
+		Set<Review> reviews = new HashSet<>();
+		Review review1 = new Review();
+		review1.setRating(5);
+		reviews.add(review1);
+		book.setReviews(reviews);
+		float averageRating = book.getAverageRating();
+		assertEquals(5.0, averageRating, 0.0);
 	}
 
 }
