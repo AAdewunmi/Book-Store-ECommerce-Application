70f4f57eccbbe623b31bdce2b7e1b3940b8e5f1d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-15 11:05:07 +0000

    Five-Hundred-Thirty-Seven Commit: Implement testAverageRating3() method in BookRatingTest class

--- test/com/bookstore/entity/BookRatingTest.java
+++ test/com/bookstore/entity/BookRatingTest.java
@@ -26,5 +26,27 @@ public class BookRatingTest {
 		float averageRating = book.getAverageRating();
 		assertEquals(0.0, averageRating, 0.0);
 	}
+	
+	@Test
+	public void testAverageRating3() {
+		Book book = new Book();
+		Set<Review> reviews = new HashSet<>();
+		
+		Review review1 = new Review();
+		review1.setRating(5);
+		reviews.add(review1);
+		
+		Review review2 = new Review();
+		review2.setRating(4);
+		reviews.add(review2);
+		
+		Review review3 = new Review();
+		review3.setRating(3);
+		reviews.add(review3);
+		
+		book.setReviews(reviews);
+		float averageRating = book.getAverageRating();
+		assertEquals(4.0, averageRating, 0.0);
+	}
 
 }
