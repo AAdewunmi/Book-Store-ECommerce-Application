ec5c6f68fb5ea09e26967179204acb8a1a974527
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-25 23:29:29 +0000

    Five-Hundred-Sixty-Two Commit: Implement doGet() method in WriteReviewServlet class

--- src/main/java/com/bookstore/controller/frontend/review/WriteReviewServlet.java
+++ src/main/java/com/bookstore/controller/frontend/review/WriteReviewServlet.java
@@ -2,13 +2,15 @@ package com.bookstore.controller.frontend.review;
 
 import java.io.IOException;
 
+import com.bookstore.service.ReviewServices;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/write_review")
+@WebServlet("write_review")
 public class WriteReviewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -17,7 +19,8 @@ public class WriteReviewServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ReviewServices reviewServices = new ReviewServices(request, response);
+		reviewServices.showReviewForm();
 	}
 
 }
