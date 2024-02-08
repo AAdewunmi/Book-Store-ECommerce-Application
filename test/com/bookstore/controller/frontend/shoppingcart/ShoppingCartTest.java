c7f8a9d86637dedae4bfbb94857dd7e777598314
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-08 08:58:32 +0000

    Five-Hundred-Eighty-Four Commit: Create skeleton ShoppingCartTest class

---
+++ test/com/bookstore/controller/frontend/shoppingcart/ShoppingCartTest.java
@@ -0,0 +1,24 @@
+package com.bookstore.controller.frontend.shoppingcart;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ShoppingCartTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
