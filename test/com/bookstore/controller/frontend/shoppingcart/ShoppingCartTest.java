9108bab056e934ee75f48070391f517201274ab7
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-13 08:42:11 +0000

    Five-Hundred-Eighty-Seven Commit: Implement testAddItem()

--- test/com/bookstore/controller/frontend/shoppingcart/ShoppingCartTest.java
+++ test/com/bookstore/controller/frontend/shoppingcart/ShoppingCartTest.java
@@ -1,24 +1,37 @@
 package com.bookstore.controller.frontend.shoppingcart;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.bookstore.entity.Book;
 
 public class ShoppingCartTest {
 
 	@Before
 	public void setUp() throws Exception {
 	}
+	
+	@Test
+	public void testAddItem() {
+		ShoppingCart cart = new ShoppingCart();
+		Book book = new Book();
+		book.setBookId(1);
+		cart.addItem(book);
+		cart.addItem(book);
+		Map<Book, Integer> items = cart.getItems(); 
+		int quantity = items.get(book);
+		assertEquals(2, quantity);
+	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	
 
 }
