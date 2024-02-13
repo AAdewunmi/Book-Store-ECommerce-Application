0013e5f5593b4cd8b8e7beba99515a71af4e9980
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-13 09:03:39 +0000

    Five-Hundred-Ninety Commit: Implement testRemoveItem2() method

--- test/com/bookstore/controller/frontend/shoppingcart/ShoppingCartTest.java
+++ test/com/bookstore/controller/frontend/shoppingcart/ShoppingCartTest.java
@@ -1,6 +1,8 @@
 package com.bookstore.controller.frontend.shoppingcart;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Map;
 
@@ -11,21 +13,34 @@ import org.junit.Test;
 import com.bookstore.entity.Book;
 
 public class ShoppingCartTest {
-
+	private static ShoppingCart cart;
 	@Before
 	public void setUp() throws Exception {
+		cart = new ShoppingCart();
+		Book book = new Book(1);
+		cart.addItem(book);
+		cart.addItem(book);
 	}
 	
 	@Test
 	public void testAddItem() {
-		ShoppingCart cart = new ShoppingCart();
-		Book book = new Book();
-		book.setBookId(1);
-		cart.addItem(book);
-		cart.addItem(book);
 		Map<Book, Integer> items = cart.getItems(); 
-		int quantity = items.get(book);
+		int quantity = items.get(new Book(1));
 		assertEquals(2, quantity);
+	}
+	
+	@Test
+	public void testRemoveItem() {
+		cart.removeItem(new Book(1));
+		assertTrue(cart.getItems().isEmpty());
+	}
+	
+	@Test
+	public void testRemoveItem2() {
+		Book book2 = new Book(2);
+		cart.addItem(book2);
+		cart.removeItem(new Book(2));
+		assertNull(cart.getItems().get(book2));
 	}
 
 	@After
