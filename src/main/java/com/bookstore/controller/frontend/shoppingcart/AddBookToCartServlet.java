1c6ece7b00f775a5c47e40a7a2c0180157faaad2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-17 12:01:45 +0000

    Six-Hundred-Five Commit: Implement doGet() method

--- src/main/java/com/bookstore/controller/frontend/shoppingcart/AddBookToCartServlet.java
+++ src/main/java/com/bookstore/controller/frontend/shoppingcart/AddBookToCartServlet.java
@@ -2,6 +2,9 @@ package com.bookstore.controller.frontend.shoppingcart;
 
 import java.io.IOException;
 
+import com.bookstore.dao.BookDAO;
+import com.bookstore.entity.Book;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,7 +21,20 @@ public class AddBookToCartServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		Integer bookId = Integer.parseInt(request.getParameter("book_id"));
+		Object cartObject = request.getSession().getAttribute("cart");
+		ShoppingCart shoppingCart = null;
+		if (cartObject != null && cartObject instanceof ShoppingCart) {
+			shoppingCart = (ShoppingCart) cartObject;
+		} else {
+			shoppingCart = new ShoppingCart();
+			request.getSession().setAttribute("cart", shoppingCart);
+		}
+		BookDAO bookDAO = new BookDAO();
+		Book book = bookDAO.get(bookId);
+		shoppingCart.addItem(book);
+		String cartPage = request.getContextPath().concat("/view_cart");
+		response.sendRedirect(cartPage);
 	}
 
 }
