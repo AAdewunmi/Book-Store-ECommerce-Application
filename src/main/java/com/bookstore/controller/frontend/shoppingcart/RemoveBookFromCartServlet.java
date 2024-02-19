86f87612817de3c1815c1dfff98d2ba358003adf
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-19 14:40:08 +0000

    Six-Hundred-Eight Commit: Create RemoveBookFromCartServlet class

---
+++ src/main/java/com/bookstore/controller/frontend/shoppingcart/RemoveBookFromCartServlet.java
@@ -0,0 +1,24 @@
+package com.bookstore.controller.frontend.shoppingcart;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/remove_from_cart")
+public class RemoveBookFromCartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public RemoveBookFromCartServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
