cd997ff4668a5f8700889f59a3c837f8ecf2389d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-23 08:41:01 +0000

    Six-Hundred-Fifteen Commit: Create skeleton ClearCartServlet class

---
+++ src/main/java/com/bookstore/controller/frontend/shoppingcart/ClearCartServlet.java
@@ -0,0 +1,24 @@
+package com.bookstore.controller.frontend.shoppingcart;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/clear_cart")
+public class ClearCartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ClearCartServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
