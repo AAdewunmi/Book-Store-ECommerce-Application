1c2c89ce2d97bc8fa9f25979ecdca87e5cf378b5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-23 08:20:05 +0000

    Six-Hundred-Fourteen Commit: Implement Update Book in Cart functionality

--- src/main/java/com/bookstore/controller/frontend/shoppingcart/UpdateCartServlet.java
+++ src/main/java/com/bookstore/controller/frontend/shoppingcart/UpdateCartServlet.java
@@ -1,6 +1,7 @@
 package com.bookstore.controller.frontend.shoppingcart;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -18,7 +19,20 @@ public class UpdateCartServlet extends HttpServlet {
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String[] arrayBookIds = request.getParameterValues("bookId");
+		String[] arrayQuantities = new String[arrayBookIds.length];
+		for (int i = 1; i <= arrayQuantities.length; i++) {
+			String aQuantity = request.getParameter("quantity" + i);
+			arrayQuantities[i - 1] = aQuantity;
+		}
+		int[] bookIds = Arrays.stream(arrayBookIds).mapToInt(Integer::parseInt).toArray();
+		int[] quantities = Arrays.stream(arrayQuantities).mapToInt(Integer::parseInt).toArray();
+		
+		ShoppingCart cart = (ShoppingCart) request.getSession().getAttribute("cart");
+		cart.updateCart(bookIds, quantities);
+		
+		String cartPage = request.getContextPath().concat("/view_cart");
+		response.sendRedirect(cartPage);	
 	}
 
 }
