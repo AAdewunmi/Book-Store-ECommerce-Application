3a002e892ba5e8ff546faca72f618f8cd8df2d4f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-28 11:26:57 +0000

    Six-Hundred-Nineteen Commit: Create skeleton OrderDAOTest class

---
+++ test/com/bookstore/dao/OrderDAOTest.java
@@ -0,0 +1,49 @@
+package com.bookstore.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class OrderDAOTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testCreateBookOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateBookOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCount() {
+		fail("Not yet implemented");
+	}
+
+}
