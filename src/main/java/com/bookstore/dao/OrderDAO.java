bff4edb69572f4135e2fd238a10dd9d3beb6be54
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-29 08:38:51 +0000

    Six-Hundred-Twenty Commit: Implement create() method

--- src/main/java/com/bookstore/dao/OrderDAO.java
+++ src/main/java/com/bookstore/dao/OrderDAO.java
@@ -1,5 +1,47 @@
 package com.bookstore.dao;
 
+import java.util.Date;
+import java.util.List;
+
+import com.bookstore.entity.BookOrder;
+
 public class OrderDAO extends JpaDAO<BookOrder> implements GenericDAO<BookOrder> {
+
+	@Override
+	public BookOrder create(BookOrder order) {
+		order.setOrderDate(new Date());		
+		order.setStatus("Processing");
+		return super.create(order);
+	}
+
+	@Override
+	public BookOrder update(BookOrder t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BookOrder get(Object id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Object id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<BookOrder> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
