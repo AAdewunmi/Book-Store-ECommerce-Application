d573825e1b1634edd320b4679b47bc2cf84ce4df
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-03-01 14:06:36 +0000

    Six-Hundred-Twenty-Five Commit: Refactor OrderDAO class

--- src/main/java/com/bookstore/dao/OrderDAO.java
+++ src/main/java/com/bookstore/dao/OrderDAO.java
@@ -10,6 +10,7 @@ public class OrderDAO extends JpaDAO<BookOrder> implements GenericDAO<BookOrder>
 	@Override
 	public BookOrder create(BookOrder order) {
 		order.setOrderDate(new Date());
+		order.setPaymentMethod("Debit Card");
 		order.setStatus("Processing");
 		return super.create(order);
 	}
