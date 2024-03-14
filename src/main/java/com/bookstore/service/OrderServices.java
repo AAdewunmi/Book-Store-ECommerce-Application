c6021a993c18cdf40bdc0335652925a8c48b6db5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-03-14 16:34:16 +0000

    Six-Hundred-Fifty-One Commit: Implement listAllOrder() method

--- src/main/java/com/bookstore/service/OrderServices.java
+++ src/main/java/com/bookstore/service/OrderServices.java
@@ -1,5 +1,35 @@
 package com.bookstore.service;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.bookstore.dao.OrderDAO;
+import com.bookstore.entity.BookOrder;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class OrderServices {
+	private OrderDAO orderDAO;
+	
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	
+	public OrderServices(HttpServletRequest request, HttpServletResponse response) {
+		super();
+		this.request = request;
+		this.response = response;
+		this.orderDAO = new OrderDAO();
+	}
+
+	public void listAllOrder() throws ServletException, IOException {
+		List<BookOrder> listOrder = orderDAO.listAll();
+		request.setAttribute("listOrder", listOrder);
+		String listPage = "order_list.jsp"; 
+		RequestDispatcher dispatcher = request.getRequestDispatcher(listPage);
+		dispatcher.forward(request, response);
+	}
 
 }
