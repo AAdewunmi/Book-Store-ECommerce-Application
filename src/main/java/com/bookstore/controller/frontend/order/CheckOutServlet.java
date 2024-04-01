7b498dd293dd6d08388865f10b694ca40b32b3db
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-04-01 08:48:35 +0100

    Six-Hundred-Sixty-Six Commit: Create CheckOutServlet class

---
+++ src/main/java/com/bookstore/controller/frontend/order/CheckOutServlet.java
@@ -0,0 +1,24 @@
+package com.bookstore.controller.frontend.order;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/checkout")
+public class CheckOutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public CheckOutServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
