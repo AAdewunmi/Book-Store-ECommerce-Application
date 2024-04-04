52a42912db8e1933b647bb93137b28987d2982c1
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-04-04 06:29:36 +0100

    Six-Hundred-Seventy-Two Commit: Create skeleton PlaceOrderServlet() method

---
+++ src/main/java/com/bookstore/controller/frontend/order/PlaceOrderServlet.java
@@ -0,0 +1,23 @@
+package com.bookstore.controller.frontend.order;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/place_order")
+public class PlaceOrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public PlaceOrderServlet() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
