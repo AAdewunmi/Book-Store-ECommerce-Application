c0d9cf5459c8f5ae2faf91e813847e178ab3c3eb
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-04-05 13:03:27 +0100

    Six-Hundred-Seventy-Four Commit: Refactor getTotalAmount() method to use float return type

--- src/main/java/com/bookstore/controller/frontend/shoppingcart/ShoppingCart.java
+++ src/main/java/com/bookstore/controller/frontend/shoppingcart/ShoppingCart.java
@@ -35,8 +35,8 @@ public class ShoppingCart {
 		return total;
 	}
 	
-	public double getTotalAmount() {
-		double total = 0.0f;
+	public float getTotalAmount() {
+		float total = 0.0f;
 		Iterator<Book> iterator = cart.keySet().iterator();
 		while (iterator.hasNext()) {
 			Book book = iterator.next();
