4550c90bc1c1c5e35618d910b924e7b1051e1e94
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-04-10 16:56:45 +0100

    Six-Hundred-Ninety-Seven Commit: Refactor CustomerLoginFilter class to handle filter view_orders and show_order_detail

--- src/main/java/com/bookstore/controller/frontend/CustomerLoginFilter.java
+++ src/main/java/com/bookstore/controller/frontend/CustomerLoginFilter.java
@@ -17,7 +17,7 @@ public class CustomerLoginFilter implements Filter {
 	
 	private static final String[] loginRequiredURLS = {
 		"/view_profile", "/edit_profile", "/update_profile", "/write_review",
-		"/checkout", "/place_order"
+		"/checkout", "/place_order", "/view_orders", "/show_order_detail"
 	};
     
 	public CustomerLoginFilter() {
