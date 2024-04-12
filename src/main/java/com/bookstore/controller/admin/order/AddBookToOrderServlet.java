f82cb6a372ba7110d2d6d9646850a40163f2ec30
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-04-12 16:49:45 +0100

    Six-Hundred-Ninety-Eight Commit: Create skeleton AddBookToOrderServlet class

---
+++ src/main/java/com/bookstore/controller/admin/order/AddBookToOrderServlet.java
@@ -0,0 +1,23 @@
+package com.bookstore.controller.admin.order;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/add_book_to_order")
+public class AddBookToOrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public AddBookToOrderServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
