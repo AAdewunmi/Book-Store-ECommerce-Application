82f3a7967cfebee62f1c3a36f940526085b599c4
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-04-13 09:15:30 +0100

    Six-Hundred-Ninety-Nine Commit: Create skeleton EditOrderServlet class

---
+++ src/main/java/com/bookstore/controller/admin/order/EditOrderServlet.java
@@ -0,0 +1,23 @@
+package com.bookstore.controller.admin.order;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/edit_order")
+public class EditOrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public EditOrderServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
