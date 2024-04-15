6f2dea9027264705d659feb17fc6694aa66c0be1
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-04-15 06:41:24 +0100

    Seven-Hundred-Nine Commit: Implement doGet() method

--- src/main/java/com/bookstore/controller/admin/order/ShowAddBookFormServlet.java
+++ src/main/java/com/bookstore/controller/admin/order/ShowAddBookFormServlet.java
@@ -1,7 +1,12 @@
 package com.bookstore.controller.admin.order;
 
 import java.io.IOException;
+import java.util.List;
 
+import com.bookstore.dao.BookDAO;
+import com.bookstore.entity.Book;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -17,7 +22,12 @@ public class ShowAddBookFormServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		BookDAO bookDAO = new BookDAO();
+		List<Book> listBook = bookDAO.listAll();
+		request.setAttribute("listBook", listBook);
+		String addFormPage = "add_book_form.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(addFormPage);
+		dispatcher.forward(request, response);
 	}
 
 }
