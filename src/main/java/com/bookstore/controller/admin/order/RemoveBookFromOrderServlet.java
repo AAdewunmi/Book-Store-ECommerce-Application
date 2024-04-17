a73579beee547f17baa4b6814a6d6fe1fc5c0161
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-04-17 08:14:41 +0100

    Seven-Hundred-Thirteen Commit: Create skeleton RemoveBookFromOrderServlet class

---
+++ src/main/java/com/bookstore/controller/admin/order/RemoveBookFromOrderServlet.java
@@ -0,0 +1,24 @@
+package com.bookstore.controller.admin.order;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/admin/remove_book_from_order")
+public class RemoveBookFromOrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public RemoveBookFromOrderServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
