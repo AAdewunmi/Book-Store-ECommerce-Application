319fab9bd6db7c06d35b11ba5155d0d39357aefa
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-04-27 10:38:20 +0100

    Seven-Hundred-Forty-Five Commit: Refactor doGet() method to handle list most recent reviews functionality

--- src/main/java/com/bookstore/controller/admin/AdminHomeServlet.java
+++ src/main/java/com/bookstore/controller/admin/AdminHomeServlet.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.util.List;
 
 import com.bookstore.dao.OrderDAO;
+import com.bookstore.dao.ReviewDAO;
 import com.bookstore.entity.BookOrder;
+import com.bookstore.entity.Review;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -30,8 +32,11 @@ public class AdminHomeServlet extends HttpServlet {
     @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		OrderDAO orderDAO = new OrderDAO();
+		ReviewDAO reviewDAO = new ReviewDAO();
 		List<BookOrder> listMostRecentSales = orderDAO.listMostRecentSales();
+		List<Review> listMostRecentReviews = reviewDAO.listMostRecent();
 		request.setAttribute("listMostRecentSales", listMostRecentSales);
+		request.setAttribute("listMostRecentReviews", listMostRecentReviews);
     	String homepage = "index.jsp";
 		RequestDispatcher dispatcher = request.getRequestDispatcher(homepage);
 		dispatcher.forward(request, response);
