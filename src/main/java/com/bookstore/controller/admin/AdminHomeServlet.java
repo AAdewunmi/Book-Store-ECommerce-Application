53ff640bd61f3e72cc4b0515def8b2c96a9e363f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-04-27 12:09:31 +0100

    Seven-Hundred-Forty-Nine Commit: Refactor doGet() to handle admin dashboard statistics

--- src/main/java/com/bookstore/controller/admin/AdminHomeServlet.java
+++ src/main/java/com/bookstore/controller/admin/AdminHomeServlet.java
@@ -3,8 +3,11 @@ package com.bookstore.controller.admin;
 import java.io.IOException;
 import java.util.List;
 
+import com.bookstore.dao.BookDAO;
+import com.bookstore.dao.CustomerDAO;
 import com.bookstore.dao.OrderDAO;
 import com.bookstore.dao.ReviewDAO;
+import com.bookstore.dao.UserDAO;
 import com.bookstore.entity.BookOrder;
 import com.bookstore.entity.Review;
 
@@ -31,13 +34,31 @@ public class AdminHomeServlet extends HttpServlet {
     
     @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		OrderDAO orderDAO = new OrderDAO();
+		UserDAO userDAO = new UserDAO();
+    	OrderDAO orderDAO = new OrderDAO();
 		ReviewDAO reviewDAO = new ReviewDAO();
+		BookDAO bookDAO = new BookDAO();
+		CustomerDAO customerDAO = new CustomerDAO();
+		
 		List<BookOrder> listMostRecentSales = orderDAO.listMostRecentSales();
 		List<Review> listMostRecentReviews = reviewDAO.listMostRecent();
+		
+		long totalUsers = userDAO.count();
+		long totalBooks = bookDAO.count();
+		long totalCustomers = customerDAO.count();
+		long totalReviews = reviewDAO.count();
+		long totalOrders = orderDAO.count();
+		
+		request.setAttribute("totalUsers", totalUsers);
+		request.setAttribute("totalBooks", totalBooks);
+		request.setAttribute("totalCustomers", totalCustomers);
+		request.setAttribute("totalReviews", totalReviews);
+		request.setAttribute("totalOrders", totalOrders);
+		
 		request.setAttribute("listMostRecentSales", listMostRecentSales);
 		request.setAttribute("listMostRecentReviews", listMostRecentReviews);
-    	String homepage = "index.jsp";
+		
+		String homepage = "index.jsp";
 		RequestDispatcher dispatcher = request.getRequestDispatcher(homepage);
 		dispatcher.forward(request, response);
 	}
