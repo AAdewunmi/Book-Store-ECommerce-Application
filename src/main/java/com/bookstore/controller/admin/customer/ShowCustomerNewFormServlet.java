884654064fbde728d7b7ee47e0872f43749de245
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-05-19 07:51:01 +0100

    Seven-Hundred-Sixty-Five Commit: Create skeleton ShowCustomerNewFormServlet class

---
+++ src/main/java/com/bookstore/controller/admin/customer/ShowCustomerNewFormServlet.java
@@ -0,0 +1,24 @@
+package com.bookstore.controller.admin.customer;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/new_customer")
+
+public class ShowCustomerNewFormServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ShowCustomerNewFormServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
