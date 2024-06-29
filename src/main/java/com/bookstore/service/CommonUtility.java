13434038329b4f47501e8a33c22ddfce5dac868f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-06-29 12:54:10 +0100

    Seven-Hundred-Ninety-Six Commit: Implement generateCountyList() method

--- src/main/java/com/bookstore/service/CommonUtility.java
+++ src/main/java/com/bookstore/service/CommonUtility.java
@@ -1,13 +1,16 @@
 package com.bookstore.service;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class CommonUtility {
-	
+	private static HttpServletRequest request;
 	public static void forwardToPage(String page, String message,
 			HttpServletRequest request, HttpServletResponse response) 
 					throws ServletException, IOException {
@@ -31,6 +34,18 @@ public class CommonUtility {
 			HttpServletRequest request, HttpServletResponse response) 
 					throws ServletException, IOException {
 		request.getRequestDispatcher(page).forward(request, response);		
-	}	
+	}
+	
+	public static void generateCountyList(HttpServletRequest request) {
+		String[] countryCodes = Locale.getISOCountries();
+		Map<String, String> mapCountries = new TreeMap<>();
+		for (String countryCode : countryCodes) {
+			Locale locale = new Locale("", countryCode);
+			String code = locale.getCountry();
+			String name = locale.getDisplayCountry();
+			mapCountries.put(name, code);
+		}
+		request.setAttribute("mapCountries", mapCountries);
+	}
 
 }
