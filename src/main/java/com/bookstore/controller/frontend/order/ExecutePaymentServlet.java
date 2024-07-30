26e925200b9077ff4f11fb8018d179e4cda3a0c5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-07-30 13:47:49 +0100

    Eight-Hundred-Twenty-Seven Commit: Implement doGet() method

--- src/main/java/com/bookstore/controller/frontend/order/ExecutePaymentServlet.java
+++ src/main/java/com/bookstore/controller/frontend/order/ExecutePaymentServlet.java
@@ -2,11 +2,16 @@ package com.bookstore.controller.frontend.order;
 
 import java.io.IOException;
 
+import com.bookstore.service.OrderServices;
+import com.bookstore.service.PaymentServices;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.transaction.Transaction;
 
 
 @WebServlet("/execute_payment")
@@ -19,7 +24,29 @@ public class ExecutePaymentServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		
+		/*
+		 * PaymentServices paymentServices = new PaymentServices(request, response);
+		 * 
+		 * try { Payment payment = paymentServices.executePayment();
+		 * 
+		 * OrderServices orderServices = new OrderServices(request, response); Integer
+		 * orderId = orderServices.placeOrderPaypal(payment);
+		 * 
+		 * HttpSession session = request.getSession(); session.setAttribute("orderId",
+		 * orderId);
+		 * 
+		 * PayerInfo payerInfo = payment.getPayer().getPayerInfo(); Transaction
+		 * transaction = payment.getTransactions().get(0);
+		 * 
+		 * session.setAttribute("payer", payerInfo); session.setAttribute("transaction",
+		 * transaction);
+		 * 
+		 * String receiptPage = "frontend/payment_receipt.jsp";
+		 * request.getRequestDispatcher(receiptPage).forward(request, response);
+		 * 
+		 * } catch (PayPalRESTException e) { e.printStackTrace(); throw new
+		 * ServletException("Error in executing payment."); }
+		 */
 	}
 
 }
